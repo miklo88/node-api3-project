058079136a17c7e405e8501e794490150ae0c9b4
Carlos <miklo.concepcion@gmail.com>
2019-12-10 19:59:25 -0600

    logger function completed. validateUserId() implementation is next.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = express();
 
 // implementing middleware or use-ing middleware
 server.use(express.json());
-server.use(helmet())
-server.use(logger)
+server.use(helmet());
+server.use(logger);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -14,16 +14,15 @@ server.get("/", (req, res) => {
 
 //custom middleware
 
+// logger middleware
+function logger(req, res, next) {
+  console.log(`${req.method} - ${req.url} - ${req.date.now()}`);
+}
+
+next();
+module.exports = server;
+
 // api built
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
 });
-
-// logger middleware
-function logger(req, res, next) {
-console.log(`${} - ${} - ${}`)
-}
-
-
-
-module.exports = server;
